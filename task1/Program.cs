b6141e246c8146c9047326a4a56c57e19ae9ee58
Andrey Zimin <andrej.zimin.1984@mail.ru>
2024-02-28 16:29:16 +0800

    Add lesson 4 dvumernyi massiv

---
+++ task1/Program.cs
@@ -0,0 +1,31 @@
+﻿int[,] CreateMatrix (int rowCount, int columnsCount) 
+{
+  int[,] matrix = new int[rowCount, columnsCount];
+
+  Random rnd = new Random();
+
+  for (int i = 0; i < matrix.GetLength(0); i++)    // matrix.GetLength(0) - требуем запросить кол-во строк в двумерном массиве
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)  // matrix.GetLength(1) -- 1 - первое измерение 
+    {
+      matrix[i, j] = rnd.Next(1, 11);
+    }
+  }
+
+  return matrix;
+}
+
+void ShowMatrix (int[,] matrix) 
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)    // matrix.GetLength(0) - требуем запросить кол-во строк в двумерном массиве
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)  // matrix.GetLength(1) -- 1 - первое измерение 
+    {
+      Console.Write($"{matrix[i, j]} ");           // Интерполяция строк
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix (matrix);
